f92aee580be631a147e41f4bcdf7ad39ea38f406
Pierre <mousset.p@husky.neu.edu>
2021-07-19 17:13:08 +0200

    (major) ingestion et pre-rporcessing

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,29 +1,43 @@
 import pandas as pd
+from pandas import json_normalize
 import pathlib
 import os
+import json
+import yaml
 
 
+def read_json_file(filename):
+    #Another option is to parse it as YAML; YAML accepts valid JSON but also accepts all sorts of variations.
+    yaml_data = yaml.safe_load(open(filename))
+    json_data = json.dumps(yaml_data)
+    dictionnary_data = json.loads(json_data)
+    dataframe = pd.DataFrame.from_dict(dictionnary_data)
+    return dataframe
+
+
+def read_csv_file(filename):
+    dataframe = pd.read_csv(filename)
+    return dataframe
+        
+        
 def read_input_file(filename):
     file_extension = pathlib.Path(filename).suffix
+    if file_extension != ".csv" and file_extension != ".json":
+        raise ValueError(f"{filename} is not supported.")
     if file_extension == '.csv':
-        df = pd.read_csv(filename)
-        print(df)
+        return read_csv_file(filename)
+    else:
+        return read_json_file(filename)
 
 
-def ingestion(input_folder):
+def extract_input_files_to_dataframes(input_folder):
+    dataframes = []
     for filename in os.listdir(input_folder):
-        read_input_file(input_folder + '/' + filename)
+        dataframe = read_input_file(input_folder + '/' + filename)
+        dataframes.append(dataframe)
+    return dataframes
+        
 
 
-ingestion('pipeline/input_data')
-
-'''
-df = pd.read_csv("./text.txt")
-df_to_doct = df.to_dict()
 
 
-a_json = json.loads(json_string)
-print(a_json)
-
-dataframe = pd.DataFrame.from_dict(a_json)
-'''
--- pipeline/validate.py
+++ pipeline/validate.py
@@ -6,6 +6,14 @@ from pandas_schema.validation import CustomElementValidation
 from decimal import *
 import datetime
 import numpy as np
+import bios
+
+
+def check_null(string):
+    if isinstance(string, float):
+        return pd.isna(string)
+    else:
+        return len(string) > 2
 
 
 def check_decimal(dec) -> boolean:
@@ -17,7 +25,7 @@ def check_decimal(dec) -> boolean:
 
 
 def check_string(string) -> boolean:
-    return  isinstance(string, str)
+    return isinstance(string, str)
 
 
 def check_date(date) -> boolean:
@@ -28,30 +36,58 @@ def check_date(date) -> boolean:
     return True
 
 
-def 
+def dataframe_to_BQ(dataframe) -> None:
+    dataframe.to_gbq('test', 
+                 Context.default().project_id,
+                 chunksize=10000, 
+                 if_exists='append',
+                 verbose=False
+                 )
 
-def do_validation(dataframe) -> pd.DataFrame:
-
-    # define validation elements
-    decimal_validation = [CustomElementValidation(lambda i: check_decimal(i), 'is not a decimal')]
-    string_validation = [CustomElementValidation(lambda i: check_string(i), 'is not a string')]
-    date_validation = [CustomElementValidation(lambda i: check_date(i), 'is not a valid date')]
-    null_validation = [CustomElementValidation(lambda i: i is not np.nan, 'this field cannot be null')]
-
-    # define validation schema
-    schema = pandas_schema.Schema([
-            Column('id', decimal_validation + null_validation),
-            Column('title', string_validation + null_validation),
-            Column('date', date_validation),
-            Column('journal', string_validation)])
-
+def do_validation(dataframe, schema) -> pd.DataFrame:
     # apply validation
     errors = schema.validate(dataframe)
     errors_index_rows = [e.row for e in errors]
     data_clean = dataframe.drop(index=errors_index_rows)
-    return dataframe
+    
+    return data_clean
 
 
+def process_input_schema(input_schema):
+    input_schema_dict = input_schema[1]
+    schemas_columns = []
+    for item in input_schema_dict:
+        name = item
+        type = input_schema_dict[item]['type']
+        is_required = input_schema_dict[item]['required']
+        column = Column(name, type, is_required)
+        print(column)
+        schemas_columns.append(column)
+    print(schemas_columns)
+        
+    
+    '''
+     # define validation elements
+    decimal_validation = [CustomElementValidation(lambda i: check_string(i), 'is not a decimal')]
+    string_validation = [CustomElementValidation(lambda i: check_string(i), 'is not a string')]
+    date_validation = [CustomElementValidation(lambda i: check_string(i), 'is not a valid date')]
+    null_validation = [CustomElementValidation(lambda i: check_null(i), 'this field cannot be null')]
+    
+    # define validation schema
+    schema = pandas_schema.Schema([
+            Column('id', string_validation + null_validation),
+            Column('scientific_title', string_validation + null_validation),
+            Column('date', string_validation),
+            Column('journal', string_validation)])
+    return schema
+    '''
 
-if __name__ == '__main__':
-    do_validation()
+def process_dataframes(dataframes):
+    input_schema = bios.read("pipeline/schemas/pubmed.yaml")
+    process_input_schema(input_schema)
+    
+    '''
+    print(dataframes[0])
+    c = do_validation(dataframes[3])
+    print(c)
+    '''
