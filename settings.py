883c797b330e571766823475c7cc8c27ca53cd57
Pierre <mousset.p@husky.neu.edu>
2021-07-22 16:53:35 +0200

    (update) removed most of the hard-coded variables

--- settings.py
+++ settings.py
@@ -1,16 +1,30 @@
 
+## General
 
-INPUT_DATA_PATH = ''
-OUTPUT_PATH = ''
+
+
+
+## Queries
+QUERY_ONE = "total_sales"
+QUERY_TWO = "sales_by_category"
+QUERY_RESULT_FOLDER = "results/sql/"
+
+## Pipeline
 DATE_FORMAT = "%Y-%m-%d"
+PIPELINE_RESULT_FOLDER_NAME = "results/pipeline/"
+PIPELINE_STORAGE_FOLDER = "storage/"
+PIPELINE_GRAPH_FILE_NAME = "graph.json"
+PIPELINE_INPUT_DATA_FOLDER = "data/"
+PIPELINE_PUBLICATIONS_FOLDER = "publications/"
+PIPELINE_DRUGS_FOLDER = "drugs/"
+PIPELINE_DATA_SCHEMA_FILENAME = "schema.yaml"
 
-
-RESULTS_FOLDER = "results/pipeline/"
+## to clean
 DRUGS_FILENAME = "drugs.json"
 CLINICAL_TRIALS_FILENAME = "clinical_trials.json"
 PUBMED_FILENAME = "pubmed.json"
 GRAPH_NAME = "graph.json"
-INPUT_DATE_COLUMN_NAME = 'date'
+INPUT_DATE_COLUMN_NAME = "date"
 INPUT_DATE_FORMAT = "%Y-%m-%d"
 FINAL_RESULT_KEYS = ["type",
                      "drug_id",
