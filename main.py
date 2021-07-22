16928bca00e45ebbb7a8e093969f901b541b0e27
Pierre <mousset.p@husky.neu.edu>
2021-07-22 15:21:20 +0200

    (fix) python format update

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ from collections import defaultdict
 parser = argparse.ArgumentParser()
 
 
-
-            
 def run_query(input_query: str):
     """Execute a SQL query on GCP BigQuery and save the result of that query on a new .json file"""
     print(input_query)
@@ -42,15 +40,12 @@ def run_query(input_query: str):
 
 def run_pipeline() -> None:
     """Execute every steeps on the pipeline and save the result in a new json file"""
-    
+
     raw_publications = extract_data('data/publications/', 'schema.yaml')
     validated_publications = validate_data(raw_publications)
     cleaned_publications = clean_data(validated_publications)
     save_data(cleaned_publications, 'results/pipeline/storage/', 'json')
     process_data('results/pipeline/storage/', 'results/pipeline/graph.json')
-
-
-    
 
 
 def run():
--- pipeline/process.py
+++ pipeline/process.py
@@ -7,20 +7,19 @@ from settings import (
     CLINICAL_TRIALS_FILENAME,
     PUBMED_FILENAME,
     GRAPH_NAME
-)  
-    
+)
 
 
 def generate_mention_drug_item(type, drug_id, drug_name, title, journal, date):
     return {
-        'type' : type,
-        'drug_id' : drug_id,
-        'drug_name' : drug_name,
-        'title' : title,
-        'journal' : journal,
-        'date' : date,
+        'type': type,
+        'drug_id': drug_id,
+        'drug_name': drug_name,
+        'title': title,
+        'journal': journal,
+        'date': date,
     }
-    
+
 
 def generate_graph(input_dic, drugs_dic):
     mention_drug_items = []
@@ -29,15 +28,15 @@ def generate_graph(input_dic, drugs_dic):
         for id, drug in drugs_dic.items():
             keyword = drug['drug']
             if keyword.lower() in target_item.lower():
-                new_mention_item = generate_mention_drug_item('pubmed', 
-                                      drug['atccode'], 
-                                      keyword, 
-                                      target_item,
-                                      journal['journal'],
-                                      journal['date']
-                                    )
+                new_mention_item = generate_mention_drug_item('pubmed',
+                                                              drug['atccode'],
+                                                              keyword,
+                                                              target_item,
+                                                              journal['journal'],
+                                                              journal['date']
+                                                              )
                 mention_drug_items.append(new_mention_item)
-    
+
     return mention_drug_items
 
 
@@ -45,17 +44,18 @@ def save_to_file(data, path):
     json_result = json.dumps(data)
     with open(path, 'w') as f:
         f.write(json_result)
-        
+
+
 def process_data(storage_path, output_path):
     drugs = {
-  "0": { "atccode": "A04AD", "drug": "DIPHENHYDRAMINE" },
-  "1": { "atccode": "S03AA", "drug": "TETRACYCLINE" },
-  "2": { "atccode": "V03AB", "drug": "ETHANOL" },
-  "3": { "atccode": "A03BA", "drug": "ATROPINE" },
-  "4": { "atccode": "A01AD", "drug": "EPINEPHRINE" },
-  "5": { "atccode": "6302001", "drug": "ISOPRENALINE" },
-  "6": { "atccode": "R01AD", "drug": "BETAMETHASONE" }
-}
+        "0": {"atccode": "A04AD", "drug": "DIPHENHYDRAMINE"},
+        "1": {"atccode": "S03AA", "drug": "TETRACYCLINE"},
+        "2": {"atccode": "V03AB", "drug": "ETHANOL"},
+        "3": {"atccode": "A03BA", "drug": "ATROPINE"},
+        "4": {"atccode": "A01AD", "drug": "EPINEPHRINE"},
+        "5": {"atccode": "6302001", "drug": "ISOPRENALINE"},
+        "6": {"atccode": "R01AD", "drug": "BETAMETHASONE"}
+    }
     for publication_type in os.listdir(storage_path):
         path = storage_path + str(publication_type)
         with open(path) as json_file:
