a441410df38872cc38be8c16d7694acf36cad8d5
Pierre <mousset.p@husky.neu.edu>
2021-07-22 09:51:41 +0200

    (udpadate) SQl fix

--- sql/sales_by_product_type.sql
+++ sql/sales_by_product_type.sql
@@ -1,7 +1,8 @@
-select s.client_id,
+SELECT s.client_id,
        sum(case when product_type = 'DECO' then s.prod_price * s.prod_qty else 0 end) deco_sales,
        sum(case when product_type = 'MEUBLE' then s.prod_price * s.prod_qty else 0 end) meuble_sales
-from  `sbx-da.medical.sales` s join
+FROM  `sbx-da.medical.sales` s join
      `sbx-da.medical.categories` p
      on s.prop_id = p.product_id
-group by s.client_id ;
+WHERE s.date between '2019-01-01' and '2019-12-31'
+GROUP by s.client_id ;
