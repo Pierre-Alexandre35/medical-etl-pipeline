5e6e4eca2d07395bfd12e766bdbd782a060305b3
Pierre <mousset.p@husky.neu.edu>
2021-07-22 10:45:21 +0200

    (major) better CLI

--- main.py
+++ main.py
@@ -5,24 +5,31 @@ from pipeline.extract import extract_input_files_to_dataframes
 from pipeline.validate import validate_dataframes
 from pipeline.cleaning import clean_dataframes
 from utils.file import save_to_file, remove_file_extension, dataframe_to_dictionary
-from settings import SQL_RESULT_FILE_EXTENSION
-import click
+from settings import FINAL_RESULT_KEYS
+import argparse
 import pandas as pd
+
+parser = argparse.ArgumentParser()
 
 
 def run_query(input_query: str):
     """Execute a SQL query on GCP BigQuery and save the result of that query on a new .json file"""
-    client = bigquery.Client()
-    try:
-        with open(input_query, "r") as text_file:
-            query_reader = text_file.read()
-        query_job = client.query(query_reader)
-        records = [dict(row) for row in query_job]
-        json_obj = json.dumps(str(records))
-        result_file_path = remove_file_extension(input_query) + SQL_RESULT_FILE_EXTENSION
-        save_to_file(json_obj, result_file_path)
-    except FileNotFoundError:
-        raise("this file does not exists")
+    print(input_query)
+    if input_query != 'total_sales' and input_query != 'sales_by_category':
+        raise ValueError("You must run either the total_sales or sales_by_category query")
+    else: 
+        client = bigquery.Client()
+        sql_file = "sql/"  + input_query + ".sql"
+        try:
+            with open(sql_file, "r") as text_file:
+                query_reader = text_file.read()
+            query_job = client.query(query_reader)
+            records = [dict(row) for row in query_job]
+            json_obj = json.dumps(str(records))
+            result_file_path = "results/sql/" + remove_file_extension(input_query) + ".json"
+            save_to_file(json_obj, result_file_path)
+        except FileNotFoundError:
+            raise("this file does not exists")
 
 
 def run_pipeline(input_folder: str, input_schemas_folder: str, result_filename: str) -> None:
@@ -38,6 +45,7 @@ def run_pipeline(input_folder: str, input_schemas_folder: str, result_filename: 
     cleaned_dataframes = clean_dataframes(validated_datafrmes)
     
     ## steep 4: save cleaned and formated data into a new folder before to process data
+    
     iterator = 0
     d = {}
     for dataframe in cleaned_dataframes:
@@ -55,7 +63,7 @@ def run_pipeline(input_folder: str, input_schemas_folder: str, result_filename: 
       
 
 
-def run(target: str, input_file_or_folder: str, *args) -> None:
+def run(target: str) -> None:
     """ run either the pipeline or SQL aueries based on the user CLI arguments """
     if target not in 'pipeline' and 'custom_queries':
         raise ValueError("The second argument must be either pipeline or sql")
@@ -65,11 +73,31 @@ def run(target: str, input_file_or_folder: str, *args) -> None:
         run_pipeline(input_file_or_folder, args[0], args[1])
 
 
-        
+
+def get_user_input():
+    parser.add_argument('--pipeline', help='')
+    parser.add_argument('--query', help='run a query')
+    parser.add_argument('--option3', help='description for option3')
+    args = parser.parse_args()
+    if not any(vars(args).values()):
+        print('You must provide some arguments to start the program. Please run main.py --help to know more about our features')
+    
+    if args.query:
+        run_query(args.query)
+
+
+
 
 if __name__ == "__main__":
-    args = sys.argv
-    globals()[args[1]](*args[2:])
+    get_user_input()
+    
+    
+
+
+        
+
+
+
 
 
 
